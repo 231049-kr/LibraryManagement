38ca43c3e817e7713fa25ea884b7aee9d95af711
231019 <231019@WINDOWS-IMN57AK.mr.hi-joho.ac.jp>
2026-06-22 09:48:30 +0900

    a

--- src/main/java/dao/BookSearchDAO.java
+++ src/main/java/dao/BookSearchDAO.java
@@ -1,6 +1,8 @@
 package dao;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class List<Book> search(String title, String name, String category) {
@@ -34,7 +36,29 @@ public class List<Book> search(String title, String name, String category) {
 		PrepareStatement ps = con.prepareStatement(sql);
 		
 		ps.setString(1, "%" + title + "%");
+		ps.setString(2, "%" + name + "%");
+		ps.setString(3, "%" + category + "%");
 		
-				
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			Book book = new Book();
+			
+			book.setId(rs.getInt("id"));
+			book.setTitle(rs.getString("title"));
+			book.setName(rs.getString("name"));
+			book.setCategory(rs.getString("category"));
+			
+			list.add(book);
+		}
+		
+		rs.close();
+		ps.close();
+		con.close();
+		
+	} catch(Exception e) {
+		e.printStackTrace();
 	}
+	
+	return list;
 }
